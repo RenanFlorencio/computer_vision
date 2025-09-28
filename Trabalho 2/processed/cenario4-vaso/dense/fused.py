8c47fa08308ce9589574e25480dc3e290c565d30
Igor Batista <igorengbatista@gmail.com>
2025-09-28 00:06:40 -0300

    imagens segmentadas e pipeline completa

--- Trabalho 2/processed/cenario4-vaso/dense/fused.py
+++
@@ -1,16 +0,0 @@
-ply
-format binary_little_endian 1.0
-element vertex 49
-property float x
-property float y
-property float z
-property float nx
-property float ny
-property float nz
-property uchar red
-property uchar green
-property uchar blue
-end_header
-mQ@pÏ@bŸVA
-†>½4r>FÔ|¿~wmÒ*s@2A@BŽRA˜º¿1x	>éòV¿ql`\ú{@þîC@r9QA˜üæ½°§œ=—œ}¿„~rÁ,€@dC@.ËQAp£Ë¾¥p=Œfj¿OK?:xs@{MJ@VXRAE-¿š‡²;Nm\¿{rAxs@\K@ÃWRAoÿ¾ú¿®;ü]¿‚~s¬_9@.C@Ô`XAÙë.>8/?ãxD¿›˜’`n@}ÙN@…DWA[Ð>"•Ý½°@h¿¶´¶p_9@ð3@Ö=XAy>Uˆ¨>²ñn¿••¨V€@†ÏO@D€PAÆí›¾;/¾(ßo¿„u¤$@èiR@`òVA\”»>~ª¾Õ)k¿¦£¦êz@\PR@/QAˆÓ½GÓK<dœ~¿……{Üí@@¶§@àWA³i+=äC>{¿Š‡~šcG@û@ƒ‰WAè¶Ž¾'â-½u¿‡ƒx)¿G@Ij@ÉWA¦áŠ¾Ú[½zv¿„€uËP9@È‰ˆ@#áRAa+¿Cú>:L,¿H=D–q5@€Øˆ@GõSA˜h)¿oß›>“d/¿I=C¬$T@·­%@èíUA¤.¿Žx·¾é(#¿~znrë7@Ó	d@‚PAûêT¿Ì†´»i ¿>24Òya@…]å?Û[TAQ;¬¾!?ÒñJ¿mifîd)@ra@AÕSAaÚ¾¡n>…Q^¿THH‚³X@	ýæ?M=UAn‚p¾Ï[?ÑaB¿m¶)O@¨ê?VATÉ ½<?7?L¿}{o4nS@6ê?‡ØUAò–1¾YŸ?P?X¿^ZO¤"f@Šè,@û˜TA]ÂC¾*5$>	çw¿tpdŠ£c@Þž-@“–TA>\éF>Êèx¿keYŒ)&@zi@·{TAãit¾x Ö¾…]`¿`QW‹J@M+î?ÛyVAÐMe½Ï?î«O¿lj^q`R@¶ó?>VAc©½ü8¾,ëz¿|xl†1*@¼â<@éYA®ýD>gKx=½z¿jekè*@H?=@JYAŽŒö=¢Nå<‚	~¿e_d¦@ü(t@¬SA<Hë=à‰‘>Õ«s¿gZa€r:@h©ü?³¤WA/¡Õ¾ñÐf¾­aa¿zxlò{Y@8·ÿ?W´UA,>¾D~µ=›|¿{wkRµL@“@™£VAÔ4I¾†M:=?½z¿sf5@n»6@iDYAy½ýn>“Íx¿PGO(¨@N	l@óSAW{Ê>1²N¾Èae¿°¥­E·,@LÄl@Û«PAm>5¿µü)¾¡º/¿I<C~¦8@bÝ@bÙWAõd»¾ì¸¨>èÌ^¿kg^¢3p@&7@ÓSA´ø¾7íp>2W¿‚|n–3u@3ÆF@Æ6RArV¾¼¢'¾ã)r¿qk]A%t@ÆF@
-:RAä§ì½í¸<:8~¿|vhí5 @r¬G@¿uWAŽvÿ=ú>#‹{¿/1.|?@Ö@£…WA£(¾¸|>wcz¿sofD®k@Ì@ñåSA¼¨ö<Ø:@=š¿Š…{=#D@Rx@q9WAÙŽ¾³d­>°n¿€~sd4D@b0@íœXA³Çg¾Ÿï–¾U©m¿mhbàXF@4Ì2@ê1XA¢õœ¾ê‚>®·j¿XRP¦#@÷Ïe@ÏÎTAåŠ¾®>®t¿m\Y
--- Trabalho 2/segmentador.py
+++ Trabalho 2/segmentador.py
@@ -7,13 +7,26 @@ from skimage.measure import find_contours
 import matplotlib.pyplot as plt
 
 BASE_PATH = "./Trabalho 2/fotos/"
-CENARIO = "cenario4-vaso/"
-IMAGES_PATH = BASE_PATH + CENARIO
-OUTPUT_PATH =  f"./Trabalho 2/seg_fotos/{CENARIO}"
+CENARIO = "boneco2"  # Altere para o cenário desejado
+IMAGES_PATH = BASE_PATH + CENARIO + "/"
+OUTPUT_PATH =  f"./Trabalho 2/seg_fotos/{CENARIO}/"
 
 os.makedirs(OUTPUT_PATH, exist_ok=True)
 
-def segmentate(img):
+# def green_superpixel_mask(img, segments):
+#     '''Cria uma máscara booleana onde True indica superpixels verdes'''
+#     mask = np.zeros(segments.shape, dtype=bool)
+#     for label in np.unique(segments):
+#         sp_mask = segments == label
+#         mean_color = img[sp_mask].mean(axis=0)
+#         # Critério simples para verde: canal G maior que R e B, e G acima de um limiar
+#         if mean_color[1] > 20 and \
+#             mean_color[1] > mean_color[0] + 20 and \
+#             mean_color[1] > mean_color[2] + 20:
+#             mask[sp_mask] = True
+#     return mask
+
+def segmentate_old(img):
     ''' Segmenta a imagem em superpixels e retorna a imagem segmentada e uma máscara binária '''
     
     segments = slic(img, n_segments=200, compactness=15, sigma=1, start_label=1)
@@ -24,23 +37,26 @@ def segmentate(img):
 
     return summary, binary, segments
 
-def green_superpixel_mask(img, segments):
-    '''Cria uma máscara booleana onde True indica superpixels verdes'''
-    mask = np.zeros(segments.shape, dtype=bool)
-    for label in np.unique(segments):
-        sp_mask = segments == label
-        mean_color = img[sp_mask].mean(axis=0)
-        # Critério simples para verde: canal G maior que R e B, e G acima de um limiar
-        if mean_color[1] > 20 and \
-            mean_color[1] > mean_color[0] + 20 and \
-            mean_color[1] > mean_color[2] + 20:
-            mask[sp_mask] = True
-    return mask
+def segmentate(img):
+    ''' Converte a imagem para o espaço LAB e segmenta via thresholding do canal A '''
+    img_lab = color.rgb2lab(img)
+    a_channel = img_lab[:, :, 1]
+    thresh_a = filters.threshold_otsu(a_channel)
+    g_mask = a_channel > thresh_a
+    l_mask = a_channel < thresh_a
+
+    if g_mask.sum() > l_mask.sum():
+        binary = l_mask
+    else:
+        binary = g_mask
+    return binary
 
 def background_removal(img, binary):
     ''' Remove o fundo da imagem usando a máscara binária '''
-    binary1 = cv2.dilate(binary.astype('uint8'), None, iterations=100)
-    binary2 = cv2.erode(binary1, None, iterations=95)
+    iterations = 20
+    margem = 5
+    binary1 = cv2.dilate(binary.astype('uint8'), None, iterations=iterations)
+    binary2 = cv2.erode(binary1, None, iterations=iterations - margem)
     mask = binary2.astype('uint8') * 255
     result = cv2.bitwise_and(img, img, mask=mask)
     return result
@@ -51,44 +67,64 @@ for name in image_names:
     if name.startswith('.'):
         continue
     img = io.imread(IMAGES_PATH + name)
-    summary, binary, segments = segmentate(img)
-    # green_mask = green_superpixel_mask(img, segments)
+    binary = segmentate(img)
     result = background_removal(img, binary)
-    # result = background_removal(img, green_mask)
     io.imsave(OUTPUT_PATH + name, result)
 
 # # Processa uma imagem específica e exibe os resultados
-# IMAGE_PATH = "./Trabalho 2/fotos/cenario4-vaso/IMG_0456.jpeg"
+# # IMAGE_PATH = "./Trabalho 2/fotos/cenario4-vaso/IMG_0505.jpeg"
+# # IMAGE_PATH = "./Trabalho 2/fotos/cenario5-bulbassauro/IMG_0668.jpeg"
+# # IMAGE_PATH = "./Trabalho 2/fotos/cenario3-dinofauro/IMG_0433.jpeg"
+# IMAGE_PATH = "./Trabalho 2/fotos/boneco-compressed/IMG_20250924_110307444.jpg"
 
 # img = io.imread(IMAGE_PATH)
-# summary, binary, segments = segmentate(img)
-# # green_mask = green_superpixel_mask(img, segments)
-# result = background_removal(img, binary)
-# # result = background_removal(img, green_mask)
 
-# plt.figure(figsize=(12, 16))
-# plt.subplot(2, 2, 1)
+# img_lab = color.rgb2lab(img)
+# binary = segmentate(img)
+
+
+# a_channel = img_lab[:, :, 1]
+# thresh_a = filters.threshold_otsu(a_channel)
+# g_mask = a_channel > thresh_a
+# l_mask = a_channel < thresh_a
+
+# if g_mask.sum() > l_mask.sum():
+#     binary = l_mask
+# else:
+#     binary = g_mask
+
+# result = background_removal(img, binary)
+
+
+# plt.figure(figsize=(18, 16))
+
+# # Exibe os canais L, A, B, a imagem original, a máscara binária e a imagem com fundo removido
+# plt.subplot(2, 3, 1)
+# plt.title('L Channel')
+# plt.imshow(img_lab[:, :, 0], cmap='gray')
+# plt.axis('off')
+
+# plt.subplot(2, 3, 2)
+# plt.title('A Channel')
+# plt.imshow(img_lab[:, :, 1], cmap='gray')
+# plt.axis('off')
+
+# plt.subplot(2, 3, 3)
+# plt.title('B Channel')
+# plt.imshow(img_lab[:, :, 2], cmap='gray')
+# plt.axis('off')
+
+# plt.subplot(2, 3, 4)
 # plt.title('Original Image')
 # plt.imshow(img)
 # plt.axis('off')
 
-# plt.subplot(2, 2, 2)
-# plt.title('Segmented Image')
-# plt.imshow(summary)
-# # plt.contour(segments, colors='red', linewidths=1)
-# plt.axis('off')
-
-# plt.subplot(2, 2, 3)
+# plt.subplot(2, 3, 5)
 # plt.title('Binary Mask')
 # plt.imshow(binary, cmap='gray')
 # plt.axis('off')
 
-# # plt.subplot(2, 2, 3)
-# # plt.title('Green Mask')
-# # plt.imshow(green_mask, cmap='gray')
-# # plt.axis('off')
-
-# plt.subplot(2, 2, 4)
+# plt.subplot(2, 3, 6)
 # plt.title('Background Removed')
 # plt.imshow(result, cmap='gray')
 # plt.axis('off')
